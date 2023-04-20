d97ec38591aa1978c6c7572b08b6e4eeb8d83f15
ChampionOfTheRealm <bostonmarathon69420@yahoo.com>
2023-04-20 10:38:55 -0400

    Created a UI for the Rock Paper Scissors game, still a work in progress but the basic visuals are working

--- main.js
+++ main.js
@@ -1,92 +1,128 @@
-//Psuedo Code:
 //Rock Paper Scissors game
 
 let playerWinCount = 0;
 let computerWinCount = 0;
 let tieCount = 0;
 
-//Generates a random choice from "rock, paper or scissors" for the computer
-function getComputerChoice() {
-    let random = Math.floor(Math.random() * 100);
-    if (random <= 33 && random >= 0) {
-        return "rock";
-    } else if (random <= 66 && random >= 34) {
-        return "paper";
-    } else if (random <= 100 && random >= 67) {
-        return "scissors";
-    }
-}
+document.getElementById("btn1").addEventListener("click", game);
+document.getElementById("btn2").addEventListener("click", game);
+document.getElementById("btn3").addEventListener("click", game);
 
-//Gets the players choice of either "rock, paper or scissor"
-function getPlayerChoice(choice = prompt("Rock, Paper or Scissors")) {
-    choice = choice.toLowerCase()
-    if (choice == "rock" || choice == "paper" || choice == "scissors") {
-        return choice;
-    } else {
-        return null;
-    }
+const roundsDiv = document.getElementById("rounds");
+const playerSelectionDiv = document.getElementById("playerSelection");
+const computerSelectionDiv = document.getElementById("computerSelection");
+const resultDiv = document.getElementById("result");
+const playerWinins = document.getElementById("playerWins");
+const computerWins = document.getElementById("computerWins");
+const ties = document.getElementById("ties");
+const winner = document.getElementById("winner");
 
-}
+roundsDiv.innerText = "Okay, lets play ";
+resultDiv.innerText = "Result: ";
+playerSelectionDiv.innerText = "Player selected ";
+computerSelectionDiv.innerText = "Computer selected "; 
+ties.innerText = "Ties: ";
+computerWins.innerText = "Computer Wins: ";
+playerWins.innerText = "Player Wins: ";
 
-//Game logic which compares the computers choice with the players choice and determines if the player or computer won or a tie and increments respective variable.
-function playGame(playerSelection, computerSelection) {
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    let result;
-
-    console.log("Player selected: " + playerSelection);
-    console.log("Computer selected: " + computerSelection);
-
-    if (playerSelection == computerSelection) {
-        tieCount++;
-        result = "Tie!";
-    } else if (playerSelection == "rock") {
-        if (computerSelection == "paper") {
-            computerWinCount++;
-            result = "Computer Wins";
-        } else if (computerSelection == "scissors") {
-            playerWinCount++;
-            result = "Player Wins!";
-            
-        }
-    } else if (playerSelection == "paper") {
-        if (computerSelection == "rock") {
-            playerWinCount++;
-            result = "Player Wins!";
-        } else if (computerSelection == "scissors") {
-            computerWinCount++;
-            result = "Computer Wins!";
-        }
-    } else if (playerSelection == "scissors") {
-        if (computerSelection == "rock") {
-            computerWinCount++;
-
-            result = "Computer Wins!";
-        } else if (computerSelection == "paper") {
-            playerWinCount++;
-            result = "Player Wins!";
-        }
-    }
-    console.log(result);
-    console.log("Ties: " + tieCount);
-    console.log("Computer Wins: " + computerWinCount);
-    console.log("Player Wins: " + playerWinCount);
-    return;
-}
 
 //Asks the player how many rounds for Rock Paper Scissors they would like to play and proceeds to call the game logic said number of times.
-function game(rounds = prompt("How many rounds would you like to play?")) {
-    rounds = parseInt(rounds);
-    console.log(typeof(rounds));
-    if (rounds >= 1 && typeof(rounds) === "number") {
-        alert("Okay, lets play " + rounds + " rounds together. Start!");
-    } else {
-        alert("Game cancelled. See ya!");
-    }
+function game(e) {
+    let playerChoice = e.target.value;
+
+    //how many rounds to play?
+    let rounds = 1;
+    //rounds = parseInt(rounds);
+    //if (rounds >= 1 && typeof(rounds) === "number") {
+    //    roundsDiv.innerText = "Okay, lets play " + rounds + " rounds together. Start!";
+    //} else {
+    //    roundsDiv.innerText ="Game cancelled. See ya!";
+    //}
     for (i = 1; i <= rounds; i++) {
         playGame();
     }
+
+    //Generates a random choice from "rock, paper or scissors" for the computer
+    function getComputerChoice() {
+        let random = Math.floor(Math.random() * 100);
+        if (random <= 33 && random >= 0) {
+            return "rock";
+        } else if (random <= 66 && random >= 34) {
+            return "paper";
+        } else if (random <= 100 && random >= 67) {
+            return "scissors";
+        }
+    }
+
+    //Game logic which compares the computers choice with the players choice and determines if the player or computer won or a tie and increments respective variable.
+    function playGame(playerSelection, computerSelection) {
+        playerSelection = playerChoice;
+        computerSelection = getComputerChoice();
+        let result;
+
+        if (playerSelection == computerSelection) {
+            tieCount++;
+            result = "Tie!";
+        } else if (playerSelection == "rock") {
+            if (computerSelection == "paper") {
+                computerWinCount++;
+                result = "Computer Wins";
+            } else if (computerSelection == "scissors") {
+                playerWinCount++;
+                result = "Player Wins!";
+
+            }
+        } else if (playerSelection == "paper") {
+            if (computerSelection == "rock") {
+                playerWinCount++;
+                result = "Player Wins!";
+            } else if (computerSelection == "scissors") {
+                computerWinCount++;
+                result = "Computer Wins!";
+            }
+        } else if (playerSelection == "scissors") {
+            if (computerSelection == "rock") {
+                computerWinCount++;
+
+                result = "Computer Wins!";
+            } else if (computerSelection == "paper") {
+                playerWinCount++;
+                result = "Player Wins!";
+            }
+        }
+
+        resultDiv.innerText = "Result: " + result;
+        playerSelectionDiv.innerText = "Player selected " + playerChoice;
+        computerSelectionDiv.innerText = "Computer selected " + computerSelection; 
+        ties.innerText = "Ties: " + tieCount;
+        computerWins.innerText = "Computer Wins: " + computerWinCount;
+        playerWins.innerText = "Player Wins: " + playerWinCount;
+
+        if (computerWinCount == 5) {
+            winner.innerText = "We have a winner! \n The Computer wins with 5 points! \n Better luck next time!";
+            computerWinCount = 0;
+            playerWinCount = 0;
+            tieCount = 0;
+        } else if (playerWinCount == 5){
+            winner.innerText = "We have a winner! \n The Player wins with 5 points! \n Congratulations!";
+            computerWinCount = 0;
+            playerWinCount = 0;
+            tieCount = 0;
+        }
+
+        return;
+    }
+    
 }
 
-//Initializes the game on web load
-game();
+function resetScore() {
+    resultDiv.innerText = "Result: ";
+    playerSelectionDiv.innerText = "Player selected ";
+    computerSelectionDiv.innerText = "Computer selected "; 
+    ties.innerText = "Ties: " + tieCount;
+    computerWins.innerText = "Computer Wins: ";
+    playerWins.innerText = "Player Wins: ";
+    computerWinCount = 0;
+    playerWinCount = 0;
+    tieCount = 0;
+}
